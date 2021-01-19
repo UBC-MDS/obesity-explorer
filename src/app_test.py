b178945f7fa0fe3fa74cbb9ed977fdd352c68876
Javairia Raza <jr.jem19@gmail.com>
2021-01-18 17:47:00 -0800

    Add initial skeleton and widgets/helper functions

---
+++ src/app_test.py
@@ -0,0 +1,111 @@
+# Load modules
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import altair as alt
+import numpy as np
+import pandas as pd
+from helper import rate
+
+# disable Altair limits
+alt.data_transformers.disable_max_rows()
+
+# read-in obesity data
+file = "data/processed/obesity-combo.csv"
+ob = pd.read_csv(file)
+
+# Instantiate the app
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H1("Top Countries"),
+        html.Iframe(
+            id="bar",
+            style={"border-width": "0", "width": "100%", "height": "500px"},
+        ),
+        dcc.Dropdown(
+            id="input_year",
+            value=2016,
+            options=[
+                {"label": year, "value": year} for year in list(ob["year"].unique())
+            ],
+        ),
+        # dcc.RadioItems(
+        #     id="input_sex",
+        #     value="Male",
+        #     options=[
+        #         {"label": sex ,"value": sex } for sex in list("Male", "Female"),
+        #     ],
+        # ),
+        dcc.RadioItems(
+            id="input_sex",
+            options=[
+                # {"label": "New York", "value": "NYC"},
+                # {"label": "Montréal", "value": "MTL"},
+                # {"label": "San Francisco", "value": "SF"},
+                {"label": sex, "value": sex}
+                for sex in ["Male", "Female"]
+            ],
+            value="Male",
+            labelStyle={"display": "inline-block"},
+        ),
+        dcc.Dropdown(
+            id="input_region",
+            value=list(ob["region"].unique()),
+            multi=True,
+            options=[
+                {"label": region, "value": region}
+                for region in list(ob["region"].unique())
+            ],
+        ),
+        dcc.Dropdown(
+            id="input_highlight_country",
+            value="Canada",
+            multi=True,
+            searchable=True,
+            options=[
+                {"label": country, "value": country}
+                for country in list(ob["country"].unique())
+            ],
+        ),
+    ]
+)
+
+
+# Bar plot
+@app.callback(Output("bar", "srcDoc"), Input("input_year", "value"))
+def plot_bar(year=0, n=20):
+    ob_yr = ob.loc[ob["year"] == year, :]
+    temp = ob_yr.groupby("country").sum("obese", "pop")
+    temp["ob_rate"] = temp["obese"] / temp["pop"]
+    ob_sorted = temp.sort_values("ob_rate", ascending=False).head(n).reset_index()
+    chart = (
+        alt.Chart(ob_sorted)
+        .mark_bar()
+        .encode(
+            x=alt.X("ob_rate", type="quantitative", title="Obesity Rate"),
+            y=alt.Y("country", sort="x", title="Country"),
+            color="ob_rate",
+            tooltip="ob_rate",
+        )
+        .interactive()
+    )
+    return chart.to_html()
+
+
+def plot_map():
+    pass
+
+
+def plot_time():
+    pass
+
+
+def plot_factor():
+    pass
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
