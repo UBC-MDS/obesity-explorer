5017e7ceab378e0ceabf28f603a62b05896e49e2
Javairia Raza <jr.jem19@gmail.com>
2021-01-20 19:27:10 -0800

    Update the srcDoc and added year input

--- src/app.py
+++ src/app.py
@@ -22,23 +22,23 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-def plot_bar(year=0, n=20):
-    ob_yr = ob.loc[ob["year"] == 2016, :]
-    temp = ob_yr.groupby("country")[["obese", "pop"]].sum()
-    temp["ob_rate"] = temp["obese"] / temp["pop"]
-    ob_sorted = temp.sort_values("ob_rate", ascending=False).head(n).reset_index()
-    chart = (
-        alt.Chart(ob_sorted)
-        .mark_bar()
-        .encode(
-            x=alt.X("ob_rate", type="quantitative", title="Obesity Rate"),
-            y=alt.Y("country", sort="x", title="Country"),
-            color="ob_rate",
-            tooltip="ob_rate",
-        )
-        .interactive()
-    )
-    return chart.to_html()
+# def plot_bar(year=0, n=20):
+#     ob_yr = ob.loc[ob["year"] == 2016, :]
+#     temp = ob_yr.groupby("country")[["obese", "pop"]].sum()
+#     temp["ob_rate"] = temp["obese"] / temp["pop"]
+#     ob_sorted = temp.sort_values("ob_rate", ascending=False).head(n).reset_index()
+#     chart = (
+#         alt.Chart(ob_sorted)
+#         .mark_bar()
+#         .encode(
+#             x=alt.X("ob_rate", type="quantitative", title="Obesity Rate"),
+#             y=alt.Y("country", sort="x", title="Country"),
+#             color="ob_rate",
+#             tooltip="ob_rate",
+#         )
+#         .interactive()
+#     )
+#     return chart.to_html()
 
 
 app.layout = html.Div(
@@ -46,7 +46,7 @@ app.layout = html.Div(
         html.H1("Top Countries"),
         html.Iframe(
             id="bar",
-            srcDoc=plot_bar(),
+            srcDoc=None,
             style={"border-width": "0", "width": "100%", "height": "500px"},
         ),
         dcc.Slider(
@@ -101,24 +101,24 @@ app.layout = html.Div(
 
 
 # Bar plot
-# @app.callback(Output("bar", "srcDoc"), Input("input_year", "value"))
-# def plot_bar(year=0, n=20):
-#     ob_yr = ob.loc[ob["year"] == 2016, :]
-#     temp = ob_yr.groupby("country").sum("obese", "pop")
-#     temp["ob_rate"] = temp["obese"] / temp["pop"]
-#     ob_sorted = temp.sort_values("ob_rate", ascending=False).head(n).reset_index()
-#     chart = (
-#         alt.Chart(ob_sorted)
-#         .mark_bar()
-#         .encode(
-#             x=alt.X("ob_rate", type="quantitative", title="Obesity Rate"),
-#             y=alt.Y("country", sort="x", title="Country"),
-#             color="ob_rate",
-#             tooltip="ob_rate",
-#         )
-#         .interactive()
-#     )
-#     return chart.to_html()
+@app.callback(Output("bar", "srcDoc"), Input("input_year", "value"))
+def plot_bar(year=0, n=20):
+    ob_yr = ob.loc[ob["year"] == year, :]
+    temp = ob_yr.groupby("country")[["obese", "pop"]].sum()
+    temp["ob_rate"] = temp["obese"] / temp["pop"]
+    ob_sorted = temp.sort_values("ob_rate", ascending=False).head(n).reset_index()
+    chart = (
+        alt.Chart(ob_sorted)
+        .mark_bar()
+        .encode(
+            x=alt.X("ob_rate", type="quantitative", title="Obesity Rate"),
+            y=alt.Y("country", sort="x", title="Country"),
+            color="ob_rate",
+            tooltip="ob_rate",
+        )
+        .interactive()
+    )
+    return chart.to_html()
 
 
 def plot_map():
